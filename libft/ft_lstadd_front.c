7b320a72b3ad9a48013bf0671ce03627f5f39940
Oscar Sericola <osericol@e4r9p1.42roma.it>
2023-04-15 16:11:49 +0200

    lst add madess

--- libft/ft_lstadd_front.c
+++ libft/ft_lstadd_front.c
@@ -6,14 +6,16 @@
 /*   By: osericol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/15 12:13:33 by osericol          #+#    #+#             */
-/*   Updated: 2023/04/15 12:27:50 by osericol         ###   ########.fr       */
+/*   Updated: 2023/04/15 16:11:32 by osericol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-
-
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
 }
