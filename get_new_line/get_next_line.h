91737f54f88d81f350c733788874d188db12b6f3
jebrich <oscar.sericola@icloud.com>
2023-04-24 15:57:24 +0200

    Started project

--- get_new_line/get_next_line.h
+++ get_new_line/get_next_line.h
@@ -6,7 +6,17 @@
 /*   By: osericol <osericol@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 12:58:11 by osericol          #+#    #+#             */
-/*   Updated: 2023/04/24 12:58:12 by osericol         ###   ########.fr       */
+/*   Updated: 2023/04/24 15:56:01 by osericol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifdef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+
+
+
+#endif
